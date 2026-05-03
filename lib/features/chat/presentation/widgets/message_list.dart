665697a92734039e541ccbfc6079277ef74a9120
Wael Aljajeh <waelaljajeh562@gmail.com>
2026-05-03 19:12:03 +0300

    feat: add auto scroll to bottom of chat when new message is sent

--- lib/features/chat/presentation/widgets/message_list.dart
+++ lib/features/chat/presentation/widgets/message_list.dart
@@ -2,19 +2,54 @@ import 'package:ecwaiproject/features/chat/domain/entities/message.dart';
 import 'package:ecwaiproject/features/chat/presentation/widgets/chat_bubble.dart';
 import 'package:flutter/material.dart';
 
-class MessageList extends StatelessWidget {
+class MessageList extends StatefulWidget {
   final List<Message> messages;
   const MessageList({super.key, required this.messages});
 
   @override
+  State<MessageList> createState() => _MessageListState();
+}
+
+class _MessageListState extends State<MessageList> {
+  final ScrollController _scrollController = ScrollController();
+  @override
+  void didUpdateWidget(covariant MessageList oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.messages.length != oldWidget.messages.length) {
+      _scrollToBottom();
+    }
+  }
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      padding: EdgeInsets.all(10),
+      controller: _scrollController,
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
       itemBuilder: (context, index) {
-        final curmessage = messages[index];
+        final curmessage = widget.messages[index];
         return ChatBubble(currentMessage: curmessage);
       },
-      itemCount: messages.length,
+      itemCount: widget.messages.length,
     );
   }
 }
